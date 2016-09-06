70f49fa981c5a55814209cb7c0571dd474278166
justingreco <justin.greco@raleighnc.gov>
2016-09-06 13:02:34 -0400

    several updates

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -43,7 +43,7 @@ angular.module('starter.controllers', [])
     for (var i = 0; i < assignment.work.length; i++) {
       features.push({attributes: {OBJECTID: assignment.work[i].oid, dueDate: assignment.dueDate}});
     }
-    $http.post('https://services.arcgis.com/v400IkDOw1ad7Yad/arcgis/rest/services/assignments_0e33b653b2d843b2a69c2e0652972c5c/FeatureServer/0/updateFeatures',
+    $http.post('https://services.arcgis.com/v400IkDOw1ad7Yad/arcgis/rest/services/assignments_1542a408cfdd45f49da345d802197905/FeatureServer/0/updateFeatures',
       {f: 'json', features: JSON.stringify(features), token: $scope.token},
       {headers: {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"},
           transformRequest: function(obj) {
@@ -67,10 +67,10 @@ angular.module('starter.controllers', [])
         $scope.user = response.data;
       }
     });
-    $http.get('https://services.arcgis.com/v400IkDOw1ad7Yad/arcgis/rest/services/workers_0e33b653b2d843b2a69c2e0652972c5c/FeatureServer/0/query?f=json&where=userId=\'' +$scope.username + '\'&outFields=*&token=' + $scope.token).then(function (response) {
+    $http.get('https://services.arcgis.com/v400IkDOw1ad7Yad/arcgis/rest/services/workers_1542a408cfdd45f49da345d802197905/FeatureServer/0/query?f=json&where=userId=\'' +$scope.username + '\'&outFields=*&token=' + $scope.token).then(function (response) {
       if (response.data.features.length > 0) {
         var oid = response.data.features[0].attributes.OBJECTID;
-        $http.get('https://services.arcgis.com/v400IkDOw1ad7Yad/arcgis/rest/services/assignments_0e33b653b2d843b2a69c2e0652972c5c/FeatureServer/0/query?f=json&orderByFields=location&where=workerId=\'' + oid + '\'&outFields=*&token=' + $scope.token).then(function (response) {
+        $http.get('https://services.arcgis.com/v400IkDOw1ad7Yad/arcgis/rest/services/assignments_1542a408cfdd45f49da345d802197905/FeatureServer/0/query?f=json&orderByFields=location&where=workerId=\'' + oid + '\'&outFields=*&token=' + $scope.token).then(function (response) {
           var d, h, m, dueTime, ampm = null;
           var item = null;
           $scope.navUrl = 'arcgis-navigator://?';
